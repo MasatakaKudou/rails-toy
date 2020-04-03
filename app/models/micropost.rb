a8affc27c2aebe945d268333c6b7a4573aa0f549
MasatakaKudou <b1017027@fun.ac.jp>
2020-04-04 07:01:57 +0900

    [add]toy appの完成

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,3 +1,7 @@
 class Micropost < ApplicationRecord
-  validates :content, length: { maximum: 140 }
+  # 1つのマイクロポストは1人のユーザーにのみ属する
+  belongs_to :user
+  # validateを実装(140を上限、内容無し)
+  validates :content, length: { maximum: 140 },
+  presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  # 一人のユーザーに複数のマイクロポストを用意
+  has_many :microposts
+  # validateを実装
+  validates :name, presence: true
+  validates :email, presence: true
 end
